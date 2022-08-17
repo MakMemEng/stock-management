447d26b4a3c6b134c1eaeda375e23ee107649348
MakMemEng <yatani.mems4568@gmail.com>
2022-08-17 12:24:27 +0900

    Show python_data to html

--- flaskr/main.py
+++ flaskr/main.py
@@ -4,6 +4,15 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    materials = [
+        {'name': 'MCL-E67',
+         'inventory': 251,
+         'price': 5017},
+        {'name': 'SPB-AK',
+         'inventory': 12,
+         'price': 684}
+    ]
     return render_template(
-        'index.html'
+        'index.html',
+        materials=materials
     )
